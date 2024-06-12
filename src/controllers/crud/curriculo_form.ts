3799a000e5aa2c696a5e8d997d5de22aa12ac333
Felipe Braun <tbraunfelipe@gmail.com>
2024-06-12 20:02:18 -0300

    correções do banco de dados e tabelas que estavam faltando

--- src/controllers/crud/curriculo_form.ts
+++ src/controllers/crud/curriculo_form.ts
@@ -19,10 +19,11 @@ async function createCurriculo(req: Request, res: Response) {
             campoEstudo,
             periodoEstudo,
             competenciasExtracurricular,
-            certificacoes
+            certificacoes,
+            empresasAntecedentes
         } = req.body;
 
-        CurriculoFormSchema.parse({ id_userCandidato, nomeEmpresa, cargo, periodo, realizacoes, instituicao, grau, campoEstudo, periodoEstudo, competenciasExtracurricular, certificacoes });
+        CurriculoFormSchema.parse({ id_userCandidato, nomeEmpresa, cargo, periodo, realizacoes, instituicao, grau, campoEstudo, periodoEstudo, competenciasExtracurricular, certificacoes, empresasAntecedentes });
 
         const verificaCandidato = await Curriculo.userCandidato.findUnique({
             where: { id_userCandidato }
@@ -42,7 +43,8 @@ async function createCurriculo(req: Request, res: Response) {
                 campoEstudo,
                 periodoEstudo,
                 competenciasExtracurricular,
-                certificacoes
+                certificacoes,
+                empresasAntecedentes
             }
         });
         return res.status(201).json(curriculoForm);
@@ -67,6 +69,7 @@ async function updateCurriculo(req: Request, res: Response) {
             periodoEstudo: z.string().min(1),
             competenciasExtracurricular: z.string().min(1),
             certificacoes: z.string().min(1),
+            empresasAntecedentes: z.string().min(1)
         }).partial(); // Torna todos os campos opcionais
 
         const parsedData = schema.safeParse(updateData);
