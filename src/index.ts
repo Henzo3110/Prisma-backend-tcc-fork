58aa7d7e5a1922140b743491ae1d15fb53eb522f
TeusCoder <mateusramalho883@gmail.com>
2024-06-12 22:53:13 -0300

    cors2

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    "origin": ["http://localhost:5173"],
+    "origin": "http://localhost:5173",
     "methods": "GET,PUT,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
